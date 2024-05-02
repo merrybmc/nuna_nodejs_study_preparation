10ab6f9cfc835c1bcb852ddca0a97a296b1d3f08
Byeong Min Cho <merrybmc@gmail.com>
2024-05-03 04:28:18 +0900

    fe setting

--- todoList/todo-be/controller/task.controller.js
+++ todoList/todo-be/controller/task.controller.js
@@ -19,7 +19,19 @@ taskController.createTask = async (req, res) => {
 
 taskController.getTask = async (req, res) => {
   try {
-    const taskList = await Task.find({}).toArray();
+    // select("-key") 해당 key는 제외하고 조회
+    const taskList = await Task.find({}).select('-__v');
+
+    res.status(200).json({ status: 'ok', data: taskList });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', error: err });
+  }
+};
+
+taskController.getTask = async (req, res) => {
+  try {
+    // select("-key") 해당 key는 제외하고 조회
+    const taskList = await Task.find({}).select('-__v');
 
     res.status(200).json({ status: 'ok', data: taskList });
   } catch (err) {
--- todoList/todo-be/model/Task.js
+++ todoList/todo-be/model/Task.js
@@ -1,17 +1,22 @@
+const { Timestamp } = require('mongodb');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // 스키마 정의
-const taskSchema = Schema({
-  task: {
-    type: String,
-    required: true,
+const taskSchema = Schema(
+  {
+    task: {
+      type: String,
+      required: true,
+    },
+    isComplete: {
+      type: Boolean,
+      required: true,
+    },
   },
-  isComplete: {
-    type: Boolean,
-    required: true,
-  },
-});
+  // 생성하는 데이터들의 생성 시간 표시
+  { timestamp: true }
+);
 
 // Task 모델 생성
 const Task = mongoose.model('Task', taskSchema);
